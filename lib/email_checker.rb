4752dddf9bda8b56977e1cb30d3aa9d5dfbfc5f4
Nicolas Marlier <nmarlier@gmail.com>
2015-04-08 18:12:30 +0200

    All response codes

--- lib/email_checker.rb
+++ lib/email_checker.rb
@@ -22,24 +22,27 @@ module EmailChecker
 
     domain = Mail::Address.new(email).domain
 
+    positive_response_codes = [250, 251]
+    negative_response_codes = [252, 421, 450, 451, 452, 500, 501, 502, 503, 504, 521, 530, 550, 551, 552, 553, 554]
+
     pop = Net::Telnet::new("Host" => server,
                            "Port" => 25,
                            "Telnetmode" => false,
-                           "Prompt" => /(250)|(550)/n)
+                           "Prompt" => Regexp.new((positive_response_codes + negative_response_codes).map{|c| "(#{c})"}.join("|")))
     response = nil
     pop.cmd("HELO john.org") { |c| response = c }
-    unless response =~ /250/
+    unless response =~ Regexp.new((positive_response_codes).map{|c| "(#{c})"}.join("|"))
       return SERVER_NOT_OK
     end
     pop.cmd("mail from:<john@john.org>")
 
     pop.cmd("rcpt to:<#{email}>") { |c| response = c }
-    unless response =~ /250/
+    unless response =~ Regexp.new((positive_response_codes).map{|c| "(#{c})"}.join("|"))
       return ADDRESS_NOT_OK
     end
 
     pop.cmd("rcpt to:<fhd7fnks78dkb@#{domain}>") { |c| response = c }
-    if response =~ /250/
+    if response =~ Regexp.new((positive_response_codes).map{|c| "(#{c})"}.join("|"))
       return ADDRESS_OK_ACCEPTS_ALL
     else
       return ADDRESS_OK
